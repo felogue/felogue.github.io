4a09c20c561a17d79a296c3b65236208053892e7
Fernando Lopez <felogue7@gmail.com>
2024-05-16 20:49:49 +0200

    Añadir js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+    function redirectFormPage() {
+        window.open("https://felogue.github.io/Free_code_camp/1-responsive-web-design/proyectos-certificacion/5-survey-form-certificacion/index.html", "_blank");
+    }
+    
+    const boton1 = document.getElementById("formPage");
+    boton1.addEventListener("click", redirectFormPage);
+
+});
