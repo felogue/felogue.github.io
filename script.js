a1fe39a3c9ea14731f326c4d939a904aa9a8a5e0
Fernando Lopez <felogue7@gmail.com>
2024-09-26 21:38:47 +0200

    html,css y js modificados para incluir CRUD

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 window.addEventListener('load', function() {
 
-    const boton1 = document.getElementById("formPage");
-    const boton2 = document.getElementById("tributePage");
+    const boton1 = document.getElementById("formRecipe");
+    const boton2 = document.getElementById("formPage");
     const boton3 = document.getElementById("technicalPage");
     const boton4 = document.getElementById("landingPage");
 
 
-    function redirectFormPage() {
-        window.open("https://felogue.github.io/Free_code_camp/1-responsive-web-design/proyectos-certificacion/5-survey-form-certificacion/index.html", "_blank");
+    function redirectFormRecipe() {
+        window.open("http://sitiofer.mywebcommunity.org/index.php", "_blank");
     }
 
-    function redirectTributePage() {
-        window.open("https://felogue.github.io/Free_code_camp/1-responsive-web-design/proyectos-certificacion/10-pagina-homenaje-certificacion/index.html", "_blank");
+    function redirectFormPage() {
+        window.open("https://felogue.github.io/Free_code_camp/1-responsive-web-design/proyectos-certificacion/5-survey-form-certificacion/index.html", "_blank");
     }
 
     function redirectTechnicalPage(){
@@ -22,8 +22,8 @@ window.addEventListener('load', function() {
         window.open("https://felogue.github.io/Free_code_camp/1-responsive-web-design/proyectos-certificacion/17-landing-page/index.html", "_blank");
     }
 
-    boton1.addEventListener("click", redirectFormPage);
-    boton2.addEventListener("click", redirectTributePage);
+    boton1.addEventListener("click", redirectFormRecipe);
+    boton2.addEventListener("click", redirectFormPage);
     boton3.addEventListener("click", redirectTechnicalPage);
     boton4.addEventListener("click", redirectLandingPage);
 
